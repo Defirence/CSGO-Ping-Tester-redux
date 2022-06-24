1c937aa34da51b4a8846e151b5288b0c20be4d7f
Defirence <33593621+Defirence@users.noreply.github.com>
2022-06-24 10:11:01 +0200

    Update main.py, README.md
    Signed-off-by: Defirence <33593621+Defirence@users.noreply.github.com>

--- README.md
+++ README.md
@@ -4,10 +4,10 @@
 
 This is the dev branch, which tracks active development.
 
-[![linting: pylint](https://img.shields.io/badge/linting-pylint-yellowgreen)](https://github.com/PyCQA/pylint) [![.github/workflows/pull_datagram.yml](https://github.com/Defirence/CSGO-Ping-Tester-redux/actions/workflows/pull_datagram.yml/badge.svg?branch=dev)](https://github.com/Defirence/CSGO-Ping-Tester-redux/actions/workflows/pull_datagram.yml)
+[![linting: pylint](https://img.shields.io/badge/linting-pylint-yellowgreen)](https://github.com/PyCQA/pylint) [![.github/workflows/pull_datagram.yml](https://github.com/Defirence/CSGO-Ping-Tester-redux/actions/workflows/pylint.yml/badge.svg?branch=dev)](https://github.com/Defirence/CSGO-Ping-Tester-redux/actions/workflows/pylint.yml)
 
 requirements.txt - Contains all the `pip` packages needed to build + run the project.
 
-main.py - The main "script", the core of what the script/program does.
+`main.py` - The main "script", the core of what the script/program does.
 
 Uses GitHub Actions for testing on `push` and `pylint` inside the Workflow for linting.
--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-import subprocess
-import requests
 import json
+import requests
+from io import open
 
-# from multiping import MultiPing
 # from multiping import multi_ping
+# from multiping import MultiPing
 
+'''csgo-ping-tester-redux-v0.0.1'''
 if __name__ == "__main__":
 
-    datagram_response = requests.get(
-    "https://raw.githubusercontent.com/SteamDatabase/SteamTracking/master/Random/NetworkDatagramConfig.json").json()
+    datagram_response = requests.get("https://tinyurl.com/steamdatagram").json()
+
+if datagram_response == 0:
+    datagram_response.close()
 
 if "certs" in datagram_response:
     del datagram_response["certs"]
@@ -25,17 +28,16 @@ if "relay_public_key" in datagram_response:
 if "typical_pings" in datagram_response:
     del datagram_response["typical_pings"]
 
-if datagram_response == 0:
-    datagram_response.close()
+with open('datagram.json', mode='w', encoding="json") as json_datagram_dump:
+    json.dump(datagram_response, json_datagram_dump)
 
-with open('datagram.json', 'w') as json_datagram:
-    json.dump(datagram_response, json_datagram)
+'''Dumps the json data from the tinyurl var and then opens the .json file for reading'''
 
 
 def print_datagram():
-    with open('datagram.json', 'r') as f:
-        d = json.load(f)
-    print(json.dumps(d, indent=4))
+    with open('datagram.json', mode='r', encoding="json") as json_datagram:
+        load_datagram = json.load(json_datagram)
+    print(json.dumps(load_datagram, indent=4))
 
 
 print("Updated datagram...\n")
