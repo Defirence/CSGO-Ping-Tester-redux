34650aeca72c4075909e172c608b74f1710409d9
Defirence <33593621+Defirence@users.noreply.github.com>
2022-06-26 16:27:08 +0200

    cleaned up if loops and added new function definition added some notes about json and the open function update .gitignore

--- main.py
+++ main.py
@@ -3,38 +3,44 @@
 from io import open
 import json
 import requests
-# from multiping import multi_ping
-# from multiping import MultiPing
 
 if __name__ == "__main__":
 
     datagram_response = requests.get("https://tinyurl.com/steamdatagram").json()
+    if datagram_response == 0:
+        datagram_response.close()
 
-if datagram_response == 0:
-    datagram_response.close()
+keywords = [
+    "revision", "certs", "p2p_share_ip",
+    "revoked_keys", "relay_public_key", "typical_pings"
+]
+for x in keywords:
+    if x in datagram_response:
+        del datagram_response[x]
 
-if "certs" in datagram_response:
-    del datagram_response["certs"]
 
-if "p2p_share_ip" in datagram_response:
-    del datagram_response["p2p_share_ip"]
+def pull_keyvalues():
+    datagram_response.get("ams")
 
-if "revoked_keys" in datagram_response:
-    del datagram_response["revoked_keys"]
 
-if "relay_public_key" in datagram_response:
-    del datagram_response["relay_public_key"]
+print(pull_keyvalues)
+# PyCharm and Python3.9.6 on Windows 11 v21H2 OS Build 22000.739 breaks with an error:
+# with open('datagram.json', mode='w', encoding='json') as json_datagram_dump:
+# LookupError: unknown encoding: json
+# This is possibly due to the .json() method being called on L9
 
-if "typical_pings" in datagram_response:
-    del datagram_response["typical_pings"]
+# Pylint will warn about using `open` here without specifying `json` encoding.
 
-with open('datagram.json', mode='w', encoding="json") as json_datagram_dump:
+# with open('datagram.json', mode='w', encoding='json') as json_datagram_dump:
+#    json.dump(datagram_response, json_datagram_dump)
+
+with open('datagram.json', mode='w') as json_datagram_dump:
     json.dump(datagram_response, json_datagram_dump)
 
 
 def print_datagram():
-    """"dumps and then opens the datagram json"""
-    with open('datagram.json', mode='r', encoding="json") as json_datagram:
+    """dumps and then opens the datagram json"""
+    with open('datagram.json', mode='r', encoding='json') as json_datagram:
         load_datagram = json.load(json_datagram)
     print(json.dumps(load_datagram, indent=4))
 
