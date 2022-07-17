8744c3cabadc6b43f6f5b5bee3be11b948c84486
Defirence <33593621+Defirence@users.noreply.github.com>
2022-07-17 14:14:49 +0200

    Add TODO and FIXME comments to main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-"""csgo-ping-tester-redux-v0.0.1-dev"""
-
+"""csgo-ping-tester-redux-v0.0.2-dev"""
 
 import sys
 from io import open
@@ -11,10 +10,11 @@ if __name__ == "__main__":
     datagram_response = requests.get("https://tinyurl.com/steamdatagram").json()
     if datagram_response == 200:
         datagram_response.close()
+# TODO: Implement a better error handing function.
 #    else:
 #        print("Error retrieving datagram from specified URL...")
 #        sys.exit(1)
-
+# FIXME: Implement a better class for different keywords and methods.
 keywords = [
     "revision", "certs", "p2p_share_ip",
     "revoked_keys", "relay_public_key", "typical_pings"
@@ -24,11 +24,13 @@ for x in keywords:
     if x in keywords:
         del datagram_response[x]
 
+# FIXME: Using open without explicit json encoding.
 with open('datagram.json', mode='w') as json_datagram_dump:
     json.dump(datagram_response, json_datagram_dump)
 
 
 def print_datagram():
+    # TODO: Missing function docstring, I really don't care less about docstrings.
     with open('datagram.json', mode='r', encoding='json') as json_datagram:
         load_datagram = json.load(json_datagram)
     print(json.dumps(load_datagram, indent=4))
@@ -37,6 +39,7 @@ def print_datagram():
 for key, value in datagram_response.items():
     print(f'{key} : {value}')
 
+# FIXME: Print the entire json response, but only values needed.
 # print(datagram_response)
 print("\nPulled updated datagram response successfully...")
 
